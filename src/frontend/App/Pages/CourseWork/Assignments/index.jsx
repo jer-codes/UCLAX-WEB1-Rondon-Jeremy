f824e074bea08116012fca7ec166a68ce14dc391
Jeremy Rondon <jrondon@g.ucla.edu>
2025-11-30 16:34:06 -0800

    Supp Lesson

--- src/frontend/App/Pages/CourseWork/Assignments/index.jsx
+++ src/frontend/App/Pages/CourseWork/Assignments/index.jsx
@@ -1,8 +1,15 @@
+//components
+import AssignmentTemplate from "./AssignmentTemplate";
+
+//markdown
+import assignment1 from "./markdown/assignment1.md?raw";
+
 // component
 const Assignments = () => {
     return (
         <div>
             <h2>Assignments</h2>
+            <AssignmentTemplate markdown={assignment1} />
         </div>
     );
 };
